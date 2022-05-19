aec317f9bd3534c731c82b8a3cc70f2e4f9f9973
Shanklesh <shanklesh.vishwakarma@hiteshi.com>
2022-05-19 17:09:21 +0530

    get all contacts endpoint

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,7 @@
 import User from "../models/users.js";
-import { getResponse } from "../util/responseObject.js";
+import { buildErrorResponse, getResponse } from "../util/responseObject.js";
 import { validationResult } from "express-validator";
+import CardProfile from "../models/cardProfile.js";
 export async function createContacts ( req, res) {
 
     try {
@@ -26,6 +27,45 @@ export async function createContacts ( req, res) {
         console.log(error)
 
         res.status(500).json(await getResponse(error,500,'Something went wrong, Please try after some time'));
-        
+
+    }
+}
+
+export async function getAllContacts( req, res) {
+
+    try {
+      let userId = req.params.userId;
+      let contacts = []; 
+      if (userId === undefined) {
+
+        res.status(400).json(await buildErrorResponse('Bad request',400,'Pleae provide userId'));
+
+      }
+
+      let user = await User.findById({_id: userId});
+
+      if (user) {
+         
+        //iterate user.contacts and get data from database
+        for (let i =0; i < user.contacts.length; i++) {
+            if (user.contacts[i]) {
+              let obj = await CardProfile.findById({_id:user.contacts[i]});
+              contacts.push(obj)
+            }
+        };
+         
+         res.status(200).json(await getResponse({contacts},200,'Contacts fetched successfully'));
+
+      }else {
+        console.log(error)
+        res.status(400).json(await buildErrorResponse('Bad request',400,'User not found'));
+
+      }
+
+    } catch (error) {
+
+        console.log(error)     
+        res.status(500).json(await buildErrorResponse(error,500,'Something went wrong, Please try after some time'));
+
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -4,32 +4,21 @@ import { verifyEmail,register, login, uploadProfileImage, updateProfile, fetchDa
 import upload from '../middlewares/upload.js';
 import auth from '../middlewares/auth.js';
 import { getAllCards } from '../controllers/cards.js';
-import { createContacts } from '../controllers/contacts.js';
+import { createContacts, getAllContacts } from '../controllers/contacts.js';
 
 const router = express.Router();
 
-router.post('/register',
-        [body('email').isEmail(),
-        body('password').isLength({min:5}),
-        body('fullName').isLength({min:1})],register);
-
-router.post('/login',[body('email').isEmail(),
-        body('password').isLength({min:1})],login);
-
-router.post('/upload-profile',auth,
-        uploadProfileImage);
-
-router.put('/update-profile', auth,[
-        body('id').isLength({min:5}),
-        body('email').isEmail(),
-        body('fullName').isLength({min:1})
-],updateProfile);
-
+router.post('/register',[body('email').isEmail(),body('password').isLength({min:5}),body('fullName').isLength({min:1})],register);
+router.post('/login',[body('email').isEmail(),body('password').isLength({min:1})],login);
+router.post('/upload-profile',auth,uploadProfileImage);
+router.put('/update-profile', auth,[body('id').isLength({min:5}),body('email').isEmail(),body('fullName').isLength({min:1})],updateProfile);
 router.get('/:userId',auth,fetchDataById);
 router.get('/',auth, fetchAllUsers)
 router.post('/verify',verifyEmail)
 router.get('/cards/:userId',auth,getAllCards);
 router.post('/contacts', auth,[body('userId').notEmpty(),body('cardId').notEmpty()], createContacts);
+router.get('/contacts/:userId',auth, getAllContacts);
+
 
 export default router;
 
